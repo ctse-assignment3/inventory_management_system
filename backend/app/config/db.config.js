967512c10492b8cd92b3f98db2e4c1e38935af2f
IT19177106 <it19177106@my.sliit.lk>
2022-05-16 22:58:45 +0530

    deployed to kubernet

--- backend/app/config/db.config.js
+++ backend/app/config/db.config.js
@@ -5,6 +5,6 @@ const {
   DB_NAME,
 } = process.env;
 module.exports = {
-  url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
-  // url: "mongodb+srv://ctseuser:ctseuser@cluster0.9yzr6.mongodb.net/inventory?retryWrites=true&w=majority"
+  // url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`
+  url: "mongodb+srv://ctseuser:ctseuser@cluster0.9yzr6.mongodb.net/inventory?retryWrites=true&w=majority"
 };
